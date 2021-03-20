fcb9777d52ef92bda9669487f84a0916a9179f54
AhmedCharfeddine <amd.99@live.fr>
2021-03-20 20:42:41 +0100

    added new layout.html file and made changes accordingly

--- app_users/urls.py
+++ app_users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views 
 urlpatterns=[
-     path('',views.index,name="index"),
+     path('',views.home,name="home"),
      path('user_login/',views.user_login,name="user_login"),
      path('user_logout/',views.user_logout,name="user_logout"),
 
